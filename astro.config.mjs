278f3d4b37f3b61156a2ec4d7ffd26e4407dd51c
calebriley <cal3briley@gmail.com>
2026-06-11 19:39:12 +0100

    Renaming folder.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,15 +10,12 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Grammar',
+					items: [{ autogenerate: { directory: 'grammar' }}],
 				},
 				{
 					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+					items: [{ autogenerate: { directory: 'reference' }}],
 				},
 			],
 		}),
