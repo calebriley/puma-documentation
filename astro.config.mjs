a33d0d946afa58e23f805868a1b1c2cc1279c3f3
calebriley <cal3briley@gmail.com>
2026-06-16 23:49:54 +0100

    Adding config for github pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://calebriley.github.io',
+	base: '/puma-documentation',
 	integrations: [
 		starlight({
 			title: 'PUMA Docs',
